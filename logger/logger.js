209ee2d14ba95c2d01c152fd848d5972884de7ed
SneakDeak Technologies <nikitakonev872@gmail.com>
2026-04-26 23:12:16 +0300

    Add files via upload

--- logger/logger.js
+++ logger/logger.js
@@ -1,1 +1,14 @@
+import chalk from 'chalk';
 
+function print(message) {
+    console.log(chalk.bold.cyan(message));
+};
+
+function error(message) {
+    console.log(chalk.bold.red(message));
+};
+
+export default {
+    print,
+    error
+};
